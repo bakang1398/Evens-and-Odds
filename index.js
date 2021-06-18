960dfdbb7aaa4e9ad1b54c37a4591fb4546726f9
bakang1398 <bakangsebetlela@gmail.com>
2021-06-18 05:39:32 +0200

    Added function to check if number is even or odd

---
+++ index.js
@@ -0,0 +1,15 @@
+function evenOrOdd(x) {
+  
+    //if the number is even
+    if (x % 2 == 0) {
+      document.write("This number is even.");
+    } 
+    
+    //if the number is odd
+    else {
+        document.write("This number is odd.");
+    }
+  }
+  
+  evenOrOdd(982);
+  evenOrOdd(789);
